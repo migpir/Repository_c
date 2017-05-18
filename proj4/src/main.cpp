5608d628d4541595b6a713e620fc269744b47444
migpir <tigerito123@yahoo.com>
2017-05-18 07:34:36 -0400

    Commits from proj4.

---
+++ proj4/src/main.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : proj3.cpp
+// Author      : Miguel H. Peralta
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	std::cout << "!?!Hello World?!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
